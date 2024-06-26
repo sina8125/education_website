c28ce8be1a739c1ed29c85a9b3f88ce277960c00
sina8125 <sina.rahimi.8125@gmail.com>
2024-06-26 14:08:31 +0330

    Revert "add local settings\nnew migration"
    This reverts commit cc333b9351a67b31bbb9741fd985d888f30de390.

--- education/local_settings.py
+++
@@ -1,8 +0,0 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d)36nz62^p@_f9w&hzt17%-)$gg%nfftzue1iz$51#m(elg-#z'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-# CSRF_TRUSTED_ORIGINS = ['']
